f5f279b3c76a399fd14152fc26da8cdf10b6f076
kriti <kriti.jindal.37@gmail.com>
2024-08-09 05:13:38 +0530

    setting up spring

---
+++ src/main/java/com/spring/basics/learning_udemy/LearningUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.spring.basics.learning_udemy;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningUdemyApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Learning Udemy Application");
+	}
+
+}
